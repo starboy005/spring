de74e05536d3da0de74999735dadd385c7d6d149
S T A R   B O Y <sujaysjsujay@gmail.com>
2023-04-22 14:58:26 +0530

    day

---
+++ sujay/src/main/java/com/example/demo/SujayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SujayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SujayApplication.class, args);
+	}
+
+}
